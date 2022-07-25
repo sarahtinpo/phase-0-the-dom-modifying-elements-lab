5bbc3d0b234399105536700d55b0496d41852cc8
Sarah Tan <sadie_red@hotmail.com>
2022-07-26 09:04:09 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
